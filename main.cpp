ddd4a6c97ab933d278bc7c8878a52468d9e09cb7
Viktor Tsvigo <11_70@bk.ru>
2024-06-06 18:18:21 +0300

    Описание коммита

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+   // std::cout << "ULLONG_MIN = " << ULLONG_ << std::endl;
+    int b=4,c=5, y=2;
+     y=y+b/c;
+    std::cout << "y = " << y << std::endl;
     std::cout << "ULLONG_MAX = " <<ULLONG_MAX<< std::endl;
       std::cout << "LLONG_MAX = "<< LLONG_MAX<<std::endl;
        std::cout << "LLONG_MIN = "<< LLONG_MIN<<std::endl;
